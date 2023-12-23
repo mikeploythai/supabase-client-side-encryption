3b63bf7666733c886ede0eefd7ab7fc81292d008
mikeploythai <110638329+mikeploythai@users.noreply.github.com>
2023-12-22 16:20:00 -0800

    created basic homepage showcasing encryption of master key

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,37 +1,110 @@
-import Link from "next/link";
+"use client";
+
+import { useState } from "react";
+import { z } from "zod";
+import { db } from "~/db";
+import {
+  decryptMasterKey,
+  encryptMasterKey,
+  generateKeyEncryptionKey,
+  generateMasterKey,
+  getPasswordKey,
+} from "~/encrypt";
+
+const schema = z.object({
+  email: z.string().email(),
+  password: z.string().min(8),
+});
 
 export default function HomePage() {
+  const [masterKey, setMasterKey] = useState("");
+  const [encryptedMasterKey, setEncryptedMasterKey] = useState("");
+  const [decryptedMasterKey, setDecryptedMasterKey] = useState("");
+
+  async function action(formData: FormData) {
+    const entries = Object.fromEntries(formData.entries());
+    const validatedEntries = schema.safeParse(entries);
+
+    if (!validatedEntries.success) {
+      for (const issue of validatedEntries.error.issues)
+        console.log(issue.message);
+      return;
+    }
+
+    const entry = await db.user
+      .filter(({ email }) => email === validatedEntries.data.email)
+      .toArray();
+
+    if (!entry.length) {
+      const passwordKey = await getPasswordKey(validatedEntries.data.password);
+      const masterKey = await generateMasterKey();
+      const salt = crypto.getRandomValues(new Uint8Array(16));
+      const keyEncryptionKey = await generateKeyEncryptionKey(
+        passwordKey,
+        salt,
+      );
+
+      if (masterKey.k) {
+        setMasterKey(masterKey.k);
+
+        const iv = crypto.getRandomValues(new Uint8Array(12));
+        const encryptedMasterKey = await encryptMasterKey(
+          keyEncryptionKey,
+          masterKey.k,
+          iv,
+        );
+
+        if (encryptedMasterKey) {
+          setEncryptedMasterKey(new TextDecoder().decode(encryptedMasterKey));
+
+          await db.user.add({
+            email: validatedEntries.data.email,
+            encryptedMasterKey,
+            salt,
+            iv,
+          });
+        }
+      }
+    } else {
+      const encryptedMasterKey = entry[0]?.encryptedMasterKey;
+      const salt = entry[0]?.salt;
+      const iv = entry[0]?.iv;
+
+      const passwordKey = await getPasswordKey(validatedEntries.data.password);
+
+      if (encryptedMasterKey && salt && iv) {
+        setEncryptedMasterKey(new TextDecoder().decode(encryptedMasterKey));
+
+        const keyEncryptionKey = await generateKeyEncryptionKey(
+          passwordKey,
+          salt,
+        );
+
+        const decryptedMasterKey = await decryptMasterKey(
+          keyEncryptionKey,
+          encryptedMasterKey,
+          iv,
+        );
+
+        if (decryptedMasterKey)
+          setDecryptedMasterKey(new TextDecoder().decode(decryptedMasterKey));
+      }
+    }
+
+    return;
+  }
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
-      <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
-        <h1 className="text-5xl font-extrabold tracking-tight text-white sm:text-[5rem]">
-          Create <span className="text-[hsl(280,100%,70%)]">T3</span> App
-        </h1>
-        <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:gap-8">
-          <Link
-            className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 text-white hover:bg-white/20"
-            href="https://create.t3.gg/en/usage/first-steps"
-            target="_blank"
-          >
-            <h3 className="text-2xl font-bold">First Steps →</h3>
-            <div className="text-lg">
-              Just the basics - Everything you need to know to set up your
-              database and authentication.
-            </div>
-          </Link>
-          <Link
-            className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 text-white hover:bg-white/20"
-            href="https://create.t3.gg/en/introduction"
-            target="_blank"
-          >
-            <h3 className="text-2xl font-bold">Documentation →</h3>
-            <div className="text-lg">
-              Learn more about Create T3 App, the libraries it uses, and how to
-              deploy it.
-            </div>
-          </Link>
-        </div>
-      </div>
+    <main>
+      <form action={action}>
+        <input id="email" name="email" className="border" />
+        <input id="password" name="password" className="border" />
+        <button type="submit">submit</button>
+      </form>
+
+      <p>Master Key: {masterKey}</p>
+      <p>Encrypted Master Key: {encryptedMasterKey}</p>
+      <p>Decrypted Master Key: {decryptedMasterKey}</p>
     </main>
   );
 }
