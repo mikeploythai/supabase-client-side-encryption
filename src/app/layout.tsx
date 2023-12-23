e9823a6351cf629b5310178dc20e700471447f00
mikeploythai <110638329+mikeploythai@users.noreply.github.com>
2023-12-23 02:43:00 -0800

    styled body and added toaster component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import "./globals.css";
 
 import { Inter } from "next/font/google";
+import { Toaster } from "./_components/ui/sonner";
 
 const inter = Inter({
   subsets: ["latin"],
@@ -20,7 +21,12 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={`font-sans ${inter.variable}`}>{children}</body>
+      <body
+        className={`flex min-h-screen flex-col items-center justify-center text-pretty bg-slate-100 font-sans antialiased ${inter.variable}`}
+      >
+        {children}
+        <Toaster richColors closeButton />
+      </body>
     </html>
   );
 }
