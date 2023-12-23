8237cd61d0ef4762b4e399daf5916ae97fa01c69
mikeploythai <110638329+mikeploythai@users.noreply.github.com>
2023-12-23 02:46:18 -0800

    created external link component

---
+++ src/app/_components/external-link.tsx
@@ -0,0 +1,26 @@
+import { ArrowTopRightIcon } from "@radix-ui/react-icons";
+import { AnchorHTMLAttributes } from "react";
+import { cn } from "~/lib/cn";
+import { buttonVariants } from "./ui/button";
+
+export default function ExternalLink({
+  href,
+  children,
+  ...props
+}: AnchorHTMLAttributes<HTMLAnchorElement>) {
+  return (
+    <a
+      href={href}
+      target="_blank"
+      rel="noreferrer"
+      className={cn(
+        buttonVariants({ variant: "link" }),
+        "h-auto gap-0.5 rounded-none p-px text-sm",
+      )}
+      {...props}
+    >
+      {children}
+      <ArrowTopRightIcon />
+    </a>
+  );
+}
